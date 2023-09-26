9e8ab0dd1a30ce1b18d60675722b55a9743faa6f
MrBabon <christophedanna06@gmail.com>
2023-09-26 15:39:26 +0200

    chatroom pour laptop termine !

--- app/javascript/controllers/chatroom_loader_controller.js
+++ app/javascript/controllers/chatroom_loader_controller.js
@@ -3,7 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="chatroom-loader"
 export default class extends Controller {
   static values = { chatroomId: Number }
-  static targets = ["messages"];
+  static targets = ["message"];
+
+  initialize() {
+    // Attendez que la page soit chargée
+    document.addEventListener("DOMContentLoaded", () => {
+      console.log("initialize() a été exécutée.");
+    });
+  }
 
   loadChatroom(event) {
     event.preventDefault();
@@ -11,10 +18,15 @@ export default class extends Controller {
     console.log(`Chatroom ID: ${chatroomId}`);
 
     if (chatroomId) {
-      fetch(`/chatrooms/${chatroomId}/messages`)
+      fetch(`/chatrooms/${chatroomId}/chatroom_partial`)
         .then((response) => response.text())
         .then((data) => {
-          this.messagesTarget.innerHTML = data;
+          const targetElement = document.querySelector(".tentative");
+
+          if (targetElement) {
+            targetElement.innerHTML = data;
+          }
+          // this.messageTarget.innerHTML = data;
         });
     } else {
       console.error("Chatroom ID is null or undefined.");
